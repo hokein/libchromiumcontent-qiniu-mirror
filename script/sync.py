fd2cb8c998b1d91a1d918116190e819050591781
Haojian Wu <hokein.wu@gmail.com>
2015-07-02 14:26:03 +0800

    Sync to qiniu platform.

--- script/sync.py
+++ script/sync.py
@@ -5,11 +5,15 @@ import os
 import re
 import subprocess
 import sys
+import tarfile
 
 SOURCE_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 REMOTE_URL = 'http://gh-contractor-zcbenz.s3.amazonaws.com/libchromiumcontent'
 CONFIG_FILE_URL = 'https://raw.githubusercontent.com/atom/electron/master/script/lib/config.py'
 SAVE_PATH = os.path.join(SOURCE_ROOT, 'download_binaries')
+VENDOR_DIR = os.path.join(SOURCE_ROOT, 'vendor')
+QINIU_TOOLS_DIR = os.path.join(VENDOR_DIR, 'qiniu-tools')
+QRSYNC_PATH = os.path.join(QINIU_TOOLS_DIR, 'qrsync')
 
 LIBCHROMIUMCONTENT_BINARIES = [
   'libchromiumcontent.zip',
@@ -25,6 +29,8 @@ TARGETS = [
   'x64',
   'ia32',
 ]
+
+QRSYNC_DOWNLOAD_URL = "http://devtools.qiniu.io/qiniu-devtools-linux_amd64-current.tar.gz"
 
 
 def execute(argv, env=os.environ):
@@ -80,11 +86,29 @@ def download(commit):
     f.write('{0}\n'.format(commit))
 
 
+def download_qrsync_if_needed():
+  if os.path.exists(QRSYNC_PATH):
+    return
+  create_dir_if_needed(QINIU_TOOLS_DIR)
+  wget_args = ['wget', QRSYNC_DOWNLOAD_URL, '-O', 'qrsync.tar.gz', '-P', VENDOR_DIR]
+  execute(wget_args)
+  with tarfile.open("qrsync.tar.gz") as tar:
+    tar.extractall(path=QINIU_TOOLS_DIR)
+
+
+def sync_to_qiniu():
+  config_path = os.path.join(VENDOR_DIR, 'conf.json')
+  qrsync_args = [QRSYNC_PATH, 'conf.json']
+  execute(qrsync_args)
+
+
 def main():
   create_dir_if_needed(SAVE_PATH)
   commit_id = get_upstream_commit_id()
   if need_download(commit_id):
     download(commit_id)
+    download_qrsync_if_needed()
+    sync_to_qiniu()
 
 
 if __name__ == '__main__':
